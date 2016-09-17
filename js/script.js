1450d5567009c06016539735e7ba7c5e63989129
Winston Jašaragić <wulsic@gmail.com>
2016-09-17 13:48:21 +0200

    Website Overhaul

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
 			  singleItem:true
 		});
 
-		//SmothScroll
+		//SmoothScroll
 		$('a[href*=\\#]').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
 			&& location.hostname == this.hostname) {
@@ -65,8 +65,6 @@ $(document).ready(function() {
 			}
 		});
 		
-		//Subscribe
-		new UIMorphingButton( document.querySelector( '.morph-button' ) );
 		// for demo purposes only
 		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
 			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
